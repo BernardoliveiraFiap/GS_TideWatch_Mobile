156d1950b46c1a7cd9b2ddeff51ffe35117a1212
BernardoliveiraFiap <RM551356@FIAP.COM.BR>
2024-05-30 01:37:32 -0300

    break

--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -1,11 +1,40 @@
 // src/screens/Homescreen.tsx
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { auth } from '../services/firebaseConfig';
+import * as Google from 'expo-auth-session/providers/google';
+import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
 
 const Homescreen = () => {
+  const [user, setUser] = useState(null);
+
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: 'YOUR_CLIENT_ID', // Your Google Client ID
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential).then((userCredential) => {
+        setUser(userCredential.user);
+      });
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Welcome to Home Screen</Text>
+      {user ? (
+        <Text>Welcome {user.displayName}</Text>
+      ) : (
+        <Button
+          title="Sign in with Google"
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+          }}
+        />
+      )}
     </View>
   );
 };
--- src/services/firebaseConfig.tsx
+++ src/services/firebaseConfig.tsx
@@ -1,18 +1,22 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  apiKey: "AIzaSyATx6fcoqiPoh2clwBeabeQ6r4a6DG6G9s",
+  authDomain: "293414242238-knkn2tv16qapiuhfqifl89d8vfbcm8l2.apps.googleusercontent.com",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_STORAGE_BUCKET",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  appId: "1:293414242238:android:9df0b2c7a880d76f771bef",
   measurementId: "YOUR_MEASUREMENT_ID",
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+let firebaseApp;
+
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
 }
 
-export { firebase };
+const auth = getAuth(firebaseApp);
+
+export { firebaseApp, auth };
